250ccfb8fc95fe5c32c0b5acfddaa07ae0ee0b70
euela <euela3979@gmail.com>
2024-08-07 08:22:36 +0300

    update render api

--- client/src/components/FilesList.jsx
+++ client/src/components/FilesList.jsx
@@ -21,7 +21,7 @@ const FilesList = () => {
 
   const getFilesList = async () => {
     try {
-      const { data } = await axios.get('https://upload-file-server-five.vercel.app/api/getAllFiles');
+      const { data } = await axios.get('https://upload-file-2.onrender.com//api/getAllFiles');
       setErrorMsg('');
       setFilesList(data);
     } catch (error) {
@@ -31,7 +31,7 @@ const FilesList = () => {
 
   const downloadFile = async (id, path, mimetype) => {
     try {
-      const result = await axios.get(`https://upload-file-server-five.vercel.app/api/download/${id}`,{
+      const result = await axios.get(`https://upload-file-2.onrender.com//api/download/${id}`,{
         responseType: 'blob'
       });
       const split = path.split('/');

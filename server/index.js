7efc0ebff8b92168fb43d483346832efabc942de
euela <euela3979@gmail.com>
2024-08-07 07:55:25 +0300

    adjust other parameters

--- server/index.js
+++ server/index.js
@@ -13,10 +13,16 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use('/',fileRouter)
+app.use('/api',fileRouter)
 // app.use('/',async(req,res)=>{
 //   res.json({message:'api working!'})
 // })
+app.use(express.static(path.join(__dirname,'..', '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'..', 'client', 'dist', 'index.html'));
+})
+
 app.listen(process.env.PORT,()=>{
     console.log(`server running on port : ${process.env.PORT}`)
 })
